49694d981e7fe7044f3e1a114a9be641b264d6f3
Eduarda Patricio <eduarda.santos.patricio@gmail.com>
2024-10-11 18:51:00 -0300

    Update funcao4.hs

--- funcao4.hs
+++ funcao4.hs
@@ -5,11 +5,12 @@ subtrairbin xs ys n = drop (length result - n) result
     subtrairComBorrow :: [Int] -> [Int] -> Int -> [Int]
     subtrairComBorrow [] [] borrow = if borrow == 0 then [] else [1]  -- Se houver um borrow sobrando no final
     subtrairComBorrow (x:xs) (y:ys) borrow =
-      let diff = x - y - borrow  -- Subtração com o borrow anterior
-          (novoBorrow, bit)
-            | diff < 0  = (1, diff + 2)  -- Se a diferença for negativa, empresta 1
-            | otherwise = (0, diff)      -- Caso contrário, não há borrow
-      in bit : subtrairComBorrow xs ys novoBorrow
+      bit : subtrairComBorrow xs ys novoBorrow
+      where
+        diff = x - y - borrow  -- Subtração com o borrow anterior
+        (novoBorrow, bit)
+          | diff < 0  = (1, diff + 2)  -- Se a diferença for negativa, empresta 1
+          | otherwise = (0, diff)      -- Caso contrário, não há borrow
     subtrairComBorrow _ _ _ = []  -- Para garantir que a recursão pare se os tamanhos das listas não baterem
 
     -- Inverte as listas, subtrai, e depois inverte o resultado
